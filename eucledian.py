a1befbe22c371956bac7801f044368e16eb49ea5
mwanjose <145431714+mwanjose@users.noreply.github.com>
2024-07-25 12:11:16 +0300

    Add files via upload

---
+++ eucledian.py
@@ -0,0 +1,11 @@
+# get the eucledian distance between (2,3) and (10,8)
+
+#formula is [(x2-x1)**2 + (y2-y1)**2]**0.5
+
+# x2 = 10, x1 = 2
+#y2 = 8, y1 = 3
+
+
+
+print(((10-2)**2 + (8-3)**2)**0.5)
+
---
+++ hello.py
@@ -0,0 +1,16 @@
+#Day 1 of 30DaysOfPython
+
+# checking data types
+
+print(type('this 30 days will have both tears and laughter')) #string
+print(type(5)) # integer
+print(type(True)) # bool
+print(type(1 + 6j)) # complex
+print(type(0.09)) # decimal
+print(type(['a','b','c'])) # list
+print(type({'a','b','c'})) # set
+print(type({'a':1,'b':2,'c':3})) # dictionary
+print(type(('a','b','c'))) # tuple
+
+
+
